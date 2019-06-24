397120a6384da86f64b62223a7d5c67708365546
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-23 18:53:24 -0700

    media: v4l2-ioctl: Use a larger on-stack video copy buffer
    We have a 16 KiB stack; a buffer of 1 KiB works perfectly fine in place
    of a small 128-byte buffer. This avoids dynamic memory allocation more
    often, improving performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -2919,8 +2919,8 @@ long
 video_usercopy(struct file *file, unsigned int cmd, unsigned long arg,
 	       v4l2_kioctl func)
 {
-	char	sbuf[128];
-	void    *mbuf = NULL, *array_buf = NULL;
+	char	sbuf[SZ_1K];
+        void    *mbuf = NULL, *array_buf = NULL;
 	void	*parg = (void *)arg;
 	long	err  = -EINVAL;
 	bool	has_array_args;
